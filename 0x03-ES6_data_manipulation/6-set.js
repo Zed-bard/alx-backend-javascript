8afb39cd3ff594139893214145c2e1c82f97e603
Zed-bard <zedbard@gmail.com>
2024-07-01 02:01:29 -0700

    6-set.js

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,7 +1,7 @@
 /**
  * Creates a set from an array.
  * @param {Array} array - The source array.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Zerihun Shiferaw <https://github.com/Zed-bard>
  * @returns {Set}
  */
 export default function setFromArray(array) {
