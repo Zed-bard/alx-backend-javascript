881e0c176b5fd108d89d2b09401a31ea127f8ef3
Zed-bard <zedbard@gmail.com>
2024-07-01 01:56:34 -0700

    2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -6,7 +6,7 @@
  *   location: String
  * }[]} students - The list of students.
  * @param {String} city - The location.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Zerihun Shiferaw <https://github.com/Zed-bard>
  * @returns
  */
 export default function getStudentsByLocation(students, city) {
